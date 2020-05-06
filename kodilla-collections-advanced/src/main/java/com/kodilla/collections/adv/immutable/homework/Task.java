8d8044169a5b3888e1dfc2e74a269a5cb1ccb956
Bartlomiej Smitkowski <bsmitkowski@gmail.com>
2020-05-06 23:14:00 +0200

    20200506 5.3. Niemutowalność

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/immutable/homework/Task.java
@@ -0,0 +1,11 @@
+package com.kodilla.collections.adv.immutable.homework;
+
+public class Task {
+    protected final String title;
+    protected final int duration;
+
+    public Task(String title, int duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+}
